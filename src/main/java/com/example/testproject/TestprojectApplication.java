9aa1549084e630bc238b3a85bcf4959b98bc800b
Jothi Kannan <jothi_kannan@hcl.com>
2025-08-26 15:06:40 +0530

    servletinitializer added

--- src/main/java/com/example/testproject/TestprojectApplication.java
+++ src/main/java/com/example/testproject/TestprojectApplication.java
@@ -2,12 +2,18 @@ package com.example.testproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestprojectApplication {
+public class TestprojectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestprojectApplication.class, args);
+	}
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestprojectApplication.class);
 	}
 
 }
